abfc8e57886846a35bfc026fbb9e994c8acd65df
harshabose <127072856+harshabose@users.noreply.github.com>
2024-03-01 04:18:00 +0530

    Delete cmake-build-debug directory

--- cmake-build-debug/test/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/harsha/Desktop/github/genetic_optimisation/test/googletest
-# Build directory: /Users/harsha/Desktop/github/genetic_optimisation/cmake-build-debug/test/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/test/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/harsha/Desktop/github/genetic_optimisation/test/googletest/googletest
-# Build directory: /Users/harsha/Desktop/github/genetic_optimisation/cmake-build-debug/test/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
